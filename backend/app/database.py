491a76d3bf4abcc2134a873dda02e82563c8cf2c
Abhishek Soni <abhisheksoni099@gmail.com>
2025-06-04 21:41:50 +0530

    Correct database connection string

--- backend/app/database.py
+++ backend/app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Update these with your MariaDB configuration
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://extuser:abhishek@sheky/hagito"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:abhishek@pibee:3306/hagito"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
